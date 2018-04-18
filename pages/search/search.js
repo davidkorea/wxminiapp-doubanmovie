b0db78b83b17be832c3b6229f78bd5f856d3fe91
davidkorea <lyfirst@naver.com>
2018-04-18 17:04:58 +0900

    function: search movie done

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,66 +1,96 @@
 // pages/search/search.js
+
+var api_url = 'https://douban.uieee.com/v2/movie/search?q=';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    movies:[]
   },
+
+  change:function(e){
+    if(!e.detail.value){
+      return;
+    }
+    wx.showToast({
+      title: 'searching...',
+      icon:'loading',
+      duration:10000
+    });
+
+    var that = this;
+    wx.request({
+      url: api_url+e.detail.value,
+      data:{},
+      header:{
+        'content-type':'json',
+      },
+      success:function(res){
+        console.log(res.data);
+        wx.hideToast();
+        that.setData({
+          movies:res.data.subjects
+        })
+      }
+    });
+  }
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  // onLoad: function (options) {
   
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  // onReady: function () {
   
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  // onShow: function () {
   
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  // onHide: function () {
   
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  // onUnload: function () {
   
-  },
+  // },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  // onPullDownRefresh: function () {
   
-  },
+  // },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  // onReachBottom: function () {
   
-  },
+  // },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  // onShareAppMessage: function () {
   
-  }
+  // }
 })
